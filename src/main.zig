cc22c0101da0c1fb8066dd5b34e570c0f5510b2b
Lucas Santos <lucascarvalhosantos91@gmail.com>
2025-05-06 01:41:00 -0300

    Init

---
+++ src/main.zig
@@ -0,0 +1,193 @@
+const std = @import("std");
+
+const Cell = struct {
+    pub const digit_count = 9;
+    pub const DigitCount = std.math.IntFittingRange(0, digit_count);
+    pub const last_digit = digit_count - 1;
+    pub const Digit = std.math.IntFittingRange(0, last_digit);
+    pub const DigitSet = std.StaticBitSet(digit_count);
+
+    possible: DigitSet = .initFull(),
+
+    pub fn count(self: @This()) DigitCount {
+        return @intCast(self.possible.count());
+    }
+    pub fn set(self: *@This(), digit: Digit) void {
+        self.possible = .initEmpty();
+        self.possible.set(@intFromEnum(digit));
+    }
+
+    pub const Status = enum {
+        ok,
+        complete,
+        invalid,
+    };
+    pub fn status(self: *@This()) Status {
+        return switch (std.math.order(self.count(), 1)) {
+            .ls => .invalid,
+            .eq => .complete,
+            .gt => .ok,
+        };
+    }
+    pub fn isComplete(self: @This()) bool {
+        return self.status() == .complete;
+    }
+    pub fn ruleOut(self: *@This(), digit: Digit) Status {
+        self.possible.unset(digit);
+        return self.status();
+    }
+    pub fn getValue(self: @This()) Digit {
+        std.debug.assert(self.isComplete());
+        return self.possible.findFirstSet().?;
+    }
+};
+
+const Grid = struct {
+    const Digit = Cell.Digit;
+    const digit_count = Cell.digit_count;
+    const cell_count = digit_count * digit_count;
+    const CellCount = std.math.IntFittingRange(0, cell_count);
+    const last_cell_index = cell_count - 1;
+    const CellIndex = std.math.IntFittingRange(0, last_cell_index);
+    const DigitSet = Cell.DigitSet;
+    const square_side = @sqrt(digit_count);
+    const last_square_index = square_side - 1;
+    const SquareIndex = std.math.IntFittingRange(0, last_square_index);
+    const CellSet = std.StaticBitSet(cell_count);
+
+    cells: [cell_count]Cell = .{} ** cell_count,
+    complete: CellSet = .initEmpty(),
+
+    const RuleOutResult = enum {
+        ok,
+        invalid,
+    };
+    fn ruleOutAndCheckIfInvalid(self: *@This(), index: CellIndex, digit: Digit, complete: *CellSet) bool {
+        switch (self.cells[index].ruleOut(digit)) {
+            .invalid => return true,
+            .ok => {},
+            .complete => complete.set(index),
+        }
+        return false;
+    }
+    fn ruleOutRow(self: *@This(), row: Digit, digit: Digit, complete: *CellSet) RuleOutResult {
+        const offset = @as(CellCount, row) * digit_count;
+        for (offset..offset + digit_count) |index| {
+            if (self.ruleOutAndCheckIfInvalid(@intCast(index), digit, complete))
+                return .invalid;
+        }
+        return .ok;
+    }
+    fn ruleOutColumn(self: *@This(), column: Digit, digit: Digit, complete: *CellSet) RuleOutResult {
+        for (0..digit_count) |n| {
+            const index = n * digit_count + column;
+            if (self.ruleOutAndCheckIfInvalid(@intCast(index), digit, complete))
+                return .invalid;
+        }
+        return .ok;
+    }
+    fn ruleOutSquare(self: *@This(), row: SquareIndex, column: SquareIndex, digit: Digit, complete: *CellSet) RuleOutResult {
+        var index = (@as(CellCount, row) * digit_count + column) * square_side;
+        for (0..square_side) |_| {
+            for (0..square_side) |_| {
+                if (self.ruleOutAndCheckIfInvalid(@intCast(index), digit, complete))
+                    return .invalid;
+                index += 1;
+            }
+            index += digit_count - square_side;
+        }
+        return .ok;
+    }
+
+    pub fn ruleOut(self: *@This(), index_arg: CellIndex) RuleOutResult {
+        var pending: CellSet = .initEmpty();
+        var index = index_arg;
+        while (true) {
+            const cell = &self.cells[index];
+            const digit = cell.getValue();
+
+            // Trick to prevent wrong .invalid
+            const backup = cell.*;
+            cell.* = .{};
+
+            const row = index / digit_count;
+            const column = index % digit_count;
+            if (self.ruleOutRow(row, digit, &pending) == .invalid) return .invalid;
+            if (self.ruleOutColumn(column, digit, &pending) == .invalid) return .invalid;
+            if (self.ruleOutSquare(row / square_side, column / square_side, digit, &pending) == .invalid) return .invalid;
+
+            cell.* = backup;
+            self.complete.set(index);
+            pending = pending.differenceWith(self.complete);
+            const next_index = pending.toggleFirstSet() orelse return .ok;
+            index = @intCast(next_index);
+        }
+    }
+};
+
+const Guess = struct {
+    const CellIndex = Grid.CellIndex;
+    grid: Grid,
+    cell: CellIndex,
+};
+
+const Clue = struct {
+    cell: Grid.CellIndex,
+    digit: Cell.Digit,
+
+    fn readByte(reader: anytype) !?u8 {
+        var char: [1]u8 = undefined;
+        const count = try reader.read(&char);
+        return if (count == 0) null else char[0];
+    }
+    fn isWhitespace(c: u8) bool {
+        return c < 0x20;
+    }
+    fn readOneNonWhitespace(reader: anytype) !?u8 {
+        while (true) {
+            const c = try readByte(reader) orelse return null;
+            if (!isWhitespace(c)) return c;
+        }
+    }
+
+    const RowError = error{InvalidRow};
+    fn parseRow(letter: u8) RowError!Cell.Digit {
+        if (letter >= 'A' and letter < 'A' + Cell.last_digit)
+            return @intCast(letter - 'A');
+        if (letter >= 'a' and letter < 'a' + Cell.last_digit)
+            return @intCast(letter - 'a');
+        return RowError.InvalidRow;
+    }
+
+    const DigitError = error{InvalidDigit};
+    fn parseDigit(num: u8) DigitError!Cell.Digit {
+        if (num >= '1' and num < '1' + Cell.last_digit)
+            return @intCast(num - '1');
+        return DigitError.InvalidDigit;
+    }
+
+    const ClueError = error{ UnexpectedEnd, InvalidColumn } || RowError || DigitError;
+    fn ParseClue(reader: anytype) ClueError!?Clue {
+        const row_letter = try readOneNonWhitespace(reader) orelse return null;
+        const row = try parseRow(row_letter);
+        const column_digit = try readByte(reader) orelse return ClueError.UnexpectedEnd;
+        const column = parseDigit(column_digit) catch |err| return if (err == DigitError.InvalidDigit)
+            ClueError.InvalidColumn
+        else
+            err;
+        const digit_char = try readByte(reader) orelse return ClueError.UnexpectedEnd;
+        const digit = try parseDigit(digit_char);
+        return .{
+            .cell = Grid.rowColToIndex(row, column),
+            .digit = digit,
+        };
+    }
+};
+
+fn setInitialGrid() void {
+    const stdin = std.io.getStdIn();
+    var buffered_stdin = std.io.bufferedReader(stdin.reader());
+    const reader = buffered_stdin.reader();
+    _ = reader; // autofix
+}
+pub fn main() void {}
